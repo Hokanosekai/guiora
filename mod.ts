f8dee9280b07e394d418de49b3d76c5a2d712a7e
Hokanosekai <arsouille.raillard@gmail.com>
2023-04-13 04:07:45 +0200

    Working using SDL2

--- mod.ts
+++ mod.ts
@@ -1,3 +1,49 @@
-const lib = Deno.dlopen
+/**
+    * Create a new window
+    *
+    * @param title The title of the window
+    * @param width The width of the window
+    * @param height The height of the window
+    * @return A pointer to the window
 
-lib.symbols.create_window();
+    #[no_mangle]
+    pub extern "C" fn window_create(title: *const u8, width: u32, height: u32) -> *mut sdl2::video::Window {
+ */
+
+const lib = Deno.dlopen("./bin/libguiora.so", {
+  window_create: {
+    parameters: ["buffer", "u32", "u32"],
+    result: "pointer",
+  },
+  window_destroy: {
+    parameters: ["pointer"],
+    result: "void",
+  },
+  window_render: {
+    parameters: ["pointer"],
+    result: "void",
+  },
+  window_update: {
+    parameters: ["pointer"],
+    result: "void",
+  },
+  window_draw_rect: {
+    parameters: ["pointer", "u32", "u32", "u32", "u32"],
+    result: "void",
+  },
+  window_clear: {
+    parameters: ["pointer"],
+    result: "void",
+  },
+});
+
+const t = new TextEncoder().encode("Hello World");
+const w = lib.symbols.window_create(t, 800, 600);
+
+lib.symbols.window_draw_rect(w, 50, 50, 100, 100);
+lib.symbols.window_render(w);
+
+while (true) {
+  lib.symbols.window_update(w);
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,124 @@
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
-use std::ptr;
+extern crate sdl2;
 
-#[no_mangle]
-pub extern "C" fn create_window() {
+use sdl2::pixels::Color;
 
-  if gtk::init().is_err() {
-    println!("Failed to initialize GTK.");
-  }
-
-  let app = Application::builder()
-    .application_id("com.example.myapp")
-    .build();
-
-  app.connect_activate(|app| {
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("My App")
-        .default_width(640)
-        .default_height(480)
-        .build();
-
-    window.show();
-  });
-
-  app.run();
+pub struct GuioraWindow {
+	pub title: String,
+	pub width: u32,
+	pub height: u32,
+	pub canvas: sdl2::render::Canvas<sdl2::video::Window>,
+	pub event_pump: sdl2::EventPump,
+	pub running: bool,
+	pub mouse_x: i32,
+	pub mouse_y: i32,
 }
 
-fn main() {
-    create_window();
+impl Drop for GuioraWindow {
+	fn drop(&mut self) {
+		println!("Dropping window");
+	}
+}
+
+
+/**
+ * Create a new window
+ *
+ * @param title (String) The title of the window
+ * @param width (Int) The width of the window
+ * @param height (Int) The height of the window
+ * @return A pointer to the window
+ */
+#[no_mangle]
+pub extern "C" fn window_create(title: *const i8, width: u32, height: u32) -> *mut GuioraWindow {
+	let title = unsafe { std::ffi::CStr::from_ptr(title).to_string_lossy().into_owned() };
+	let sdl_context = sdl2::init().unwrap();
+	let video_subsystem = sdl_context.video().unwrap();
+	let window = video_subsystem.window(&title, width, height)
+		.position_centered()
+		.opengl()
+		.build()
+		.unwrap();
+	let canvas = window.into_canvas().build().unwrap();
+	let event_pump = sdl_context.event_pump().unwrap();
+	let mut guiora = GuioraWindow {
+		title: title,
+		width: width,
+		height: height,
+		canvas: canvas,
+		event_pump: event_pump,
+		running: true,
+		mouse_x: 0,
+		mouse_y: 0,
+	};
+
+	// Set the background color
+	guiora.canvas.set_draw_color(Color::RGB(0, 0, 0));
+	guiora.canvas.clear();
+	guiora.canvas.present();
+
+	// Return a pointer to the window
+	Box::into_raw(Box::new(guiora))
+}
+
+#[no_mangle]
+pub extern "C" fn window_destroy(window: *mut GuioraWindow) {
+	unsafe {
+		drop(Box::from_raw(window));
+	}
+}
+
+#[no_mangle]
+pub extern "C" fn window_render(window: *mut GuioraWindow) {
+	unsafe {
+		(*window).canvas.present();
+	}
+}
+
+#[no_mangle]
+pub extern "C" fn window_update(window: *mut GuioraWindow) {
+	unsafe {
+		println!("Mouse moved to ({}, {})", (*window).mouse_x, (*window).mouse_y);
+		for event in (*window).event_pump.poll_iter() {
+			match event {
+				sdl2::event::Event::Quit {..} => {
+					(*window).running = false;
+					// Kill the window
+					window_destroy(window);
+				},
+				sdl2::event::Event::MouseMotion { x, y, .. } => {
+					(*window).mouse_x = x;
+					(*window).mouse_y = y;
+				},
+				_ => {}
+			}
+		}
+	}
+}
+
+#[no_mangle]
+pub extern "C" fn window_draw_rect(window: *mut GuioraWindow, x: i32, y: i32, width: u32, height: u32) {
+	unsafe {
+		println!("Drawing rect at ({}, {})", x, y);
+		println!("Drawing rect with width {} and height {}", width, height);
+		(*window).canvas.set_draw_color(Color::RGB(255, 0, 0));
+		(*window).canvas.fill_rect(sdl2::rect::Rect::new(x, y, width, height)).unwrap();
+	}
+}
+
+#[no_mangle]
+pub extern "C" fn window_clear(window: *mut GuioraWindow) {
+	unsafe {
+		(*window).canvas.clear();
+	}
+}
+
+#[no_mangle]
+pub extern "C" fn window_set_title(window: *mut GuioraWindow, title: *const i8) {
+	let title = unsafe { std::ffi::CStr::from_ptr(title).to_string_lossy().into_owned() };
+	unsafe {
+		(*window).title = title;
+	}
+}
+
+pub fn main() {
 }
